f3d4fb1cb7a7bf2081ae677dfaf112e7f3dcbf21
jkolb <jkolb@teikametrics.com>
2018-04-27 11:07:20 -0400

    Removing uneeded locale config

--- src/utils/defaultLocale.js
+++ src/utils/defaultLocale.js
@@ -1,20 +1,6 @@
 module.exports = {
   blank: 'Select a date...',
   headerFormat: 'ddd, MMM Do',
-  monthLabels: [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ],
   todayLabel: {
     long: 'Today',
   },
